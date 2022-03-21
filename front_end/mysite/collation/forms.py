4c566489b697b7f911188044868fe9a7de8bccf6
jon-ashley <jonashley@law.virginia.edu>
2022-03-21 08:16:16 -0400

    Undo backend/frontend split.

--- front_end/mysite/collation/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class DataQueryForm(forms.Form):
-    civil = forms.BooleanField(label='civil', required=False)
-    criminal = forms.BooleanField(label='criminal', required=False)
